78b7301e08d1f748ffc49edd93002f6e515e5de6
apiip0709 <afifalhaq777@gmail.com>
2023-06-08 22:46:57 +0800

    Smtp dan Imap

---
+++ apkemail.py
@@ -0,0 +1,76 @@
+import smtplib
+from tkinter import Text, Tk, Label, Entry, Button, messagebox
+
+def send_email():
+    gmail_user = gmail_entry.get()
+    gmail_pass = password_entry.get()
+    sent_to = to_entry.get()
+    sent_subject = subject_entry.get()
+    sent_body = body_entry.get("1.0", "end-1c")
+
+    email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (gmail_user, sent_to, sent_subject, sent_body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_pass)
+        server.sendmail(gmail_user, sent_to, email_text)
+        server.close()
+
+        messagebox.showinfo("Email Sent", "Email berhasil dikirim!")
+    except Exception as exception:
+        messagebox.showerror("Error", f"Error: {exception}")
+
+# Membuat jendela aplikasi menggunakan Tkinter
+window = Tk()
+window.title("Aplikasi Pengiriman Email")
+window.geometry("400x300")
+
+# Label dan Entry untuk akun Gmail
+gmail_label = Label(window, text="Akun Gmail:")
+gmail_label.pack()
+gmail_entry = Entry(window, width=30)
+gmail_entry.pack()
+
+# Label dan Entry untuk password Gmail
+password_label = Label(window, text="Password Gmail:")
+password_label.pack()
+password_entry = Entry(window, show="*", width=30)
+password_entry.pack()
+
+# Label dan Entry untuk penerima email
+to_label = Label(window, text="Gmail Penerima:")
+to_label.pack()
+to_entry = Entry(window, width=30)
+to_entry.pack()
+
+# Label dan Entry untuk subjek email
+subject_label = Label(window, text="Subject:")
+subject_label.pack()
+subject_entry = Entry(window, width=30)
+subject_entry.pack()
+
+# Label dan Entry untuk isi pesan email
+body_label = Label(window, text="Pesan:")
+body_label.pack()
+body_entry = Text(window)
+body_entry.pack()
+
+# Tombol Kirim Email
+send_button = Button(window, text="Kirim", command=send_email)
+send_button.pack()
+
+# Menjalankan aplikasi
+window.mainloop()
+
+    # gmail_user = "tugas.smtplib@gmail.com"
+    # gmail_pass = "jpqvzhoqujptemlv"
+
+        # gmail_user = "tugas.imaplib@gmail.com"
+        # gmail_pass = "htqtgddtcxeyxvdo"  
---
+++ app.py
@@ -0,0 +1,158 @@
+import smtplib
+import imaplib
+import email
+from flask import Flask, render_template, request, session, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = "secret_key"
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_cek():
+    gmail_user = request.form['gmail_user']
+    gmail_pass = request.form['gmail_pass']
+
+    try:
+        # Autentikasi SMTP (untuk mengirim email)
+        smtp_obj = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp_obj.starttls()
+        smtp_obj.login(gmail_user, gmail_pass)
+
+        # Autentikasi IMAP (untuk membaca email)
+        imap_obj = imaplib.IMAP4_SSL('imap.gmail.com')
+        imap_obj.login(gmail_user, gmail_pass)
+
+        # Simpan informasi autentikasi ke dalam sesi
+        session['gmail_user'] = gmail_user
+        session['gmail_pass'] = gmail_pass
+
+        return redirect(url_for('home'))
+
+    except smtplib.SMTPAuthenticationError:
+        # Tampilkan pesan error jika autentikasi gagal
+        return render_template('login.html', error="Login gagal! Email atau password salah.", action="/login")
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        return render_template('home.html')
+    else:
+        return render_template('home.html')
+
+@app.route('/received_emails')
+def received_emails():
+    try:
+        gmail_user = session.get('gmail_user')
+        gmail_pass = session.get('gmail_pass')
+
+        if not gmail_user or not gmail_pass:
+            return render_template('error.html', error_message="Please login to access emails.")
+
+        # Mengakses email menggunakan IMAP
+        mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        mail.login(gmail_user, gmail_pass)
+        mail.select('inbox')
+
+        # Mencari semua email dalam kotak masuk
+        result, data = mail.search(None, 'ALL')
+        email_ids = data[0].split()
+        email_ids = list(reversed(email_ids))  # Membalik urutan email_ids
+
+        email_content = []  # Memindahkan inisialisasi list di luar loop
+
+        if email_ids:
+            for email_id in email_ids:
+                result, data = mail.fetch(email_id, '(RFC822)')
+                raw_email = data[0][1]
+
+                # Parsing email
+                email_message = email.message_from_bytes(raw_email)
+
+                # Mendapatkan informasi pengirim
+                from_address = email.utils.parseaddr(email_message['From'])[1]
+
+                # Menampilkan informasi email
+                email_info = {
+                    'From': from_address,
+                    'To': email_message['To'],
+                    'Subject': email_message['Subject'],
+                    'Body': ''
+                }
+
+                # Mengekstrak isi pesan
+                if email_message.is_multipart():
+                    for part in email_message.walk():
+                        content_type = part.get_content_type()
+                        if content_type == 'text/plain':
+                            body = part.get_payload(decode=True).decode()
+                            email_info['Body'] = body
+                else:
+                    body = email_message.get_payload(decode=True).decode()
+                    email_info['Body'] = body
+
+                email_content.append(email_info)
+
+            mail.close()
+            mail.logout()
+
+            return render_template('received_emails.html', emails=email_content)
+        else:
+            mail.close()
+            mail.logout()
+            return render_template('received_emails.html', emails=[])
+    except Exception as e:
+        return render_template('error.html', error_message=f"Error: {e}")
+
+@app.route('/send_email', methods=['GET', 'POST'])
+def send_email():
+    if request.method == 'POST':
+        # Mengambil informasi pengguna dari session
+        gmail_user = session.get('gmail_user')
+        gmail_pass = session.get('gmail_pass')
+
+        if not gmail_user or not gmail_pass:
+            # Jika pengguna belum login, tampilkan pesan kesalahan
+            return render_template('error.html', error_message="Please login to send emails.")
+
+        # Mengambil informasi dari formulir yang dikirimkan pengguna
+        sent_from = gmail_user
+        sent_to = request.form['sent_to']
+        sent_subject = request.form['sent_subject']
+        sent_body = request.form['sent_body']
+
+        # Membuat objek email_message dan mengatur pengirim, penerima, subjek, dan isi email
+        email_message = email.message.EmailMessage()
+        email_message['From'] = sent_from
+        email_message['To'] = sent_to
+        email_message['Subject'] = sent_subject
+        email_message.set_content(sent_body)
+
+        try:
+            # Mengirim email menggunakan server SMTP Gmail
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(gmail_user, gmail_pass)
+            server.send_message(email_message)
+            server.quit()
+
+            # Jika email berhasil dikirim, render template success.html
+            return render_template('success.html')
+        except Exception as exception:
+            # Jika terjadi kesalahan saat mengirim email, tampilkan pesan kesalahan
+            error_message = str(exception)
+            return render_template('error.html', error_message=error_message)
+
+    # Metode GET untuk mengakses halaman formulir
+    return render_template('send_email.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('gmail_user', None)
+    session.pop('gmail_pass', None)
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
